aab2535108530e85e036ff9fec05de2d862a70aa
meeshxD <lo.michelle0810@gmail.com>
2024-10-19 16:32:14 -0400

    Assignment 3 - Prime Factors
    Assignment 3 - Prime Factor

---
+++ prime.py
@@ -0,0 +1,26 @@
+import pytest
+def generate_prime_factors(n):
+    prime_factors = []
+    prime_factors.append(n)
+    if n == 2:
+        return prime_factors
+    if n>2:
+        sign = 1
+        x = 2
+        while sign == 1:
+            if x == prime_factors[-1]:
+                sign = 0
+            elif n % x == 0:
+                if prime_factors[-1] % x == 0:
+                    prime_factors.insert(-1,x)
+                    y = prime_factors[-1]/x
+                    prime_factors.insert(-1, int(y))
+                    prime_factors.pop()
+                    x = 2
+                else:
+                    x += 1
+                    sign = 1
+            else:
+                x += 1
+    return prime_factors
+
---
+++ test_prime_factors.py
@@ -0,0 +1,5 @@
+import unittest
+from prime import generate_prime_factors
+class test_prime(unittest.TestCase)
+def test_data_prime_factors():
+    assert prime.generate_prime_factors(2) == [2]
